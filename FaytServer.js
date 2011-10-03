dc6317a54b1615a768c7d54423a9c23d702e3c81
Burak SARICA <bsarica@hotmail.com>
2011-10-03 15:54:17 +0300

    first try

--- FaytServer.js
+++ FaytServer.js
@@ -1,30 +1,6 @@
-var clientFiles = new static.Server('./FaytClient');
-
-var httpServer = http.createServer(function(request, response) {
-    request.addListener('end', function () {
-        clientFiles.serve(request, response);
-    });
-});
-httpServer.listen(9999);
-
-var webSocket = socketIO.listen(httpServer);
-webSocket.on('connection', function(client) {
-    client.send('Please enter a user name ...');
-
-    var userName;
-    client.on('message', function(message) {
-        if(!userName) {
-            userName = message;
-            webSocket.broadcast(message + ' has entered the zone.');
-            return;
-        }
-
-        var broadcastMessage = userName + ': ' + message;
-        webSocket.broadcast(broadcastMessage);
-    });
-
-    client.on('disconnect', function() {
-        var broadcastMessage = userName + ' has left the zone.';
-        webSocket.broadcast(broadcastMessage);
-    });
-});
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World Nodejs run on Amazon EC2 by fyhao\n');
+}).listen(80);
+console.log('Server running on Amazon EC2');
