c7dcac5f902c034220df12222b31996cb4f88638
Burak SARICA <bsarica@hotmail.com>
2011-10-12 15:45:03 +0300

    ip added

--- FaytServer.js
+++ FaytServer.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var connect = require('connect');
 
-var server = connect(
+var server = connect.createServer(
   connect.logger()
   , connect.static(__dirname + '/FaytClient')
 ).listen(80,"10.228.182.253");
