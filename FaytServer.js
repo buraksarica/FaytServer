d2ad9a9effb0a4d087ab5b79af7c8570d86244f1
Burak SARICA <bsarica@hotmail.com>
2011-10-12 15:18:55 +0300

    ip added

--- FaytServer.js
+++ FaytServer.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World Nodejs run on Amazon EC2 by fyhao\n');
-}).listen(9999,"10.228.182.253");
+}).listen(1337,"10.228.182.253");
 console.log('Server running on Amazon EC2');
