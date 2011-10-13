2bbebf79dba91dafdb4e34b2791480543592f195
Burak SARICA <bsarica@hotmail.com>
2011-10-13 08:46:00 +0300

    ip added

--- FaytServer.js
+++ FaytServer.js
@@ -8,22 +8,22 @@ var server = connect.createServer(
 
 var webSocket = require('socket.io').listen(1337,"10.228.182.253");
 webSocket.on('connection', function(client) {
-    client.emit('Please enter a user name ...');
+    client.send('Please enter a user name ...');
 
     var userName;
     client.on('message', function(message) {
         if(!userName) {
             userName = message;
-            webSocket.broadcast(message + ' has entered the zone.');
+            webSocket.broadcast.emit(message + ' has entered the zone.');
             return;
         }
 
         var broadcastMessage = userName + ': ' + message;
-        webSocket.broadcast(broadcastMessage);
+        webSocket.broadcast.send(broadcastMessage);
     });
 
     client.on('disconnect', function() {
         var broadcastMessage = userName + ' has left the zone.';
-        webSocket.broadcast(broadcastMessage);
+        webSocket.broadcast.send(broadcastMessage);
     });
 });
