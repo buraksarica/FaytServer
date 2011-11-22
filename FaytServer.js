20918befb2302d7044100a13e1cf34596b0e44a2
Burak SARICA <bsarica@hotmail.com>
2011-11-22 15:07:05 +0200

    hebelek

--- FaytServer.js
+++ FaytServer.js
@@ -3,27 +3,5 @@ var connect = require('connect');
 
 var server = connect.createServer(
   connect.logger()
-  , connect.static(__dirname + '/FaytClient')
+  , connect.static(__dirname + '/')
 ).listen(80,"10.228.182.253");
-
-var webSocket = require('socket.io').listen(1337,"127.0.0.1");
-webSocket.on('connection', function(client) {
-    client.send('Please enter a user name ...');
-
-    var userName;
-    client.on('message', function(message) {
-        if(!userName) {
-            userName = message;
-            webSocket.broadcast.emit(message + ' has entered the zone.');
-            return;
-        }
-
-        var broadcastMessage = userName + ': ' + message;
-        webSocket.broadcast.send(broadcastMessage);
-    });
-
-    client.on('disconnect', function() {
-        var broadcastMessage = userName + ' has left the zone.';
-        webSocket.broadcast.send(broadcastMessage);
-    });
-});
